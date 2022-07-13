22d17532420cd8715ebd30d1d6fa2c29e6b9ecd6
David Arques <david.darqsa@gmail.com>
2022-07-13 18:58:12 +0200

    Added calculator-html

---
+++ public/projects/calculator-html/index.js
@@ -0,0 +1,27 @@
+
+const screen = document.getElementById("screen")
+const AC = document.getElementById("reset")
+const del = document.getElementById("delete")
+const div = document.getElementById("/")
+const mul = document.getElementById("*")
+const res = document.getElementById("-")
+const sum = document.getElementById("+")
+const equal = document.getElementById("=")
+const decimal = document.getElementById(".")
+const btn1 = document.getElementById("1")
+const btn2 = document.getElementById("2")
+const btn3 = document.getElementById("3")
+const btn4 = document.getElementById("4")
+const btn5 = document.getElementById("5")
+const btn6 = document.getElementById("6")
+const btn7 = document.getElementById("7")
+const btn8 = document.getElementById("8")
+const btn9 = document.getElementById("9")
+const btn0 = document.getElementById("0")
+
+// funciones a hacer:
+// enseñar num en la pantalla 
+// operaciones
+//checkear decimales
+//checkear si hay el maximo de nums
+//event listeners: numeros, coma, reset, operaciones, borrar, igual
